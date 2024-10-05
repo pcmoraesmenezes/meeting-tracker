78e051586561ff8429c2fc2fa1845ab02a920780
pcmoraesmenezes <paulo.moraes@sou.unifal-mg.edu.br>
2024-10-05 17:27:25 -0300

    :sparkles: feat: Ajustando a lógica para ter campos não obrigatorios

--- meeting/tracker_app/forms.py
+++ meeting/tracker_app/forms.py
@@ -30,6 +30,7 @@ class TrackerForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        
         # Obter o ID do status "ativo"
         try:
             active_status = Status.objects.get(name='ativo')
@@ -37,5 +38,26 @@ class TrackerForm(forms.ModelForm):
             self.fields['client'].queryset = Client.objects.filter(id_status=active_status.id)
         except Status.DoesNotExist:
             self.fields['client'].queryset = Client.objects.none()
+        
         # Tornar `did_they_show` um dropdown com todas as opções
         self.fields['did_they_show'].queryset = DidTheyShow.objects.all()
+        
+        # Definir os campos obrigatórios
+        self.fields['client'].required = True
+        self.fields['date_scheduled'].required = True
+        self.fields['company_name'].required = True
+        self.fields['first_name'].required = True
+        self.fields['last_name'].required = True
+        self.fields['email'].required = True
+        self.fields['lu_rep'].required = True
+
+        # Definir os campos não obrigatórios
+        self.fields['did_they_show'].required = False
+        self.fields['meeting_date'].required = False
+        self.fields['meeting_time'].required = False
+        self.fields['time_zone'].required = False
+        self.fields['appointment_handler'].required = False
+        self.fields['position'].required = False
+        self.fields['phone_number'].required = False
+        self.fields['lu_call_notes'].required = False
+        self.fields['call_recording_link'].required = False
